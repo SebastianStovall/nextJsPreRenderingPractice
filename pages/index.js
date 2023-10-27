01b565fe8fb6e59eba7edaa6d4dcb450bd7fea6f
SebastianStovall <sebastianstovall@gmail.com>
2023-10-27 12:47:45 -0500

    added SSG for home page with firebase dummy backend

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,37 @@
-import { getFeaturedEvents } from "../dummy-data"
+
+// new import using data from an actual backend
+import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "../components/events/event-list";
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+// this component props in data received from the backend so it can be displayed with the EventList component
+// the content for the events is now rendered with the HTML page -----> https://i.gyazo.com/bd13cd145bd4b66283ad0c3cc45b9cca.png
+function HomePage(props) {
+  const events = props.events
 
   return (
     <div>
         <h1 style={{textAlign: "center"}}>Featured Events</h1>
-        <EventList items={featuredEvents} />
+        <EventList items={events} />
     </div>
   )
+}
+
+
+// use Static Generation for Home Page. Opt for static generation over SSR because content is not that dyanamic.
+// Definetly want a pre-rendered page for the home page for search crawlers
+
+// fetch the data from the firebase database so it can be pre-rendered
+export async function getStaticProps(context) {
+
+  // this helper function can be located in the helpers folder under apiutil.js
+  const featuredEvents = await getFeaturedEvents()
+
+  return {
+    props: {
+      events: featuredEvents
+    }
+  }
+
 }
 
 export default HomePage;
